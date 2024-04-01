38c88a5af55dd0afd908280a23811c2eb5863688
compuvalpo@gmail.com <compuvalpo@gmail.com>
2024-03-31 22:09:50 -0300

    Add Tooltip
    Usando libreria Popper

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
 
 	const clipboard = new ClipboardJS('.btnClip');
+
+	clipboard.on('success', function(e) {
+
+		const id = '#' + e.trigger.getAttribute('id');
+		const tooltip = bootstrap.Tooltip.getInstance(id);
+		
+		e.trigger.setAttribute('data-bs-original-title','Copiado');
+		e.trigger.classList.add('btn-success');
+		tooltip.show();
+		
+	});
 });
 
 function SwalMensaje(title, text, icon, url) {
@@ -35,21 +46,27 @@ function GenerarHashHmac(){
 						
 						$('.card-data').html("");
 						$('.card-data').append(div +'MD5</span><span class="form-control" id="md5">'+ data.md5 +'</span>'
-						+'<button class="input-group-text btnClip" id="btnMD5" data-clipboard-text="'+ data.md5 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						+'<button class="input-group-text btnClip" id="btnMD5" data-clipboard-text="'+ data.md5 +'" data-bs-toggle="tooltip" data-bs-title="Copiar"><i class="fa-regular fa-copy"></i></button></div>');
 						
 						$('.card-data').append(div +'SHA1</span><span class="form-control" id="sha1">'+ data.sha1 +'</span>'
-						+'<button class="input-group-text btnClip" id="btnSHA1" data-clipboard-text="'+ data.sha1 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						+'<button class="input-group-text btnClip" id="btnSHA1" data-clipboard-text="'+ data.sha1 +'" data-bs-toggle="tooltip" data-bs-title="Copiar"><i class="fa-regular fa-copy"></i></button></div>');
 						
 						$('.card-data').append(div +'SHA256</span><span class="form-control" id="sha256">'+ data.sha256 +'</span>'
-						+'<button class="input-group-text btnClip" id="btnSHA256" data-clipboard-text="'+ data.sha256 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						+'<button class="input-group-text btnClip" id="btnSHA256" data-clipboard-text="'+ data.sha256 +'" data-bs-toggle="tooltip" data-bs-title="Copiar"><i class="fa-regular fa-copy"></i></button></div>');
 						
 						$('.card-data').append(div +'SHA512</span><span class="form-control" id="sha512">'+ data.sha512 +'</span>'
-						+'<button class="input-group-text btnClip" id="btnSHA512" data-clipboard-text="'+ data.sha512 +'"><i class="fa-regular fa-copy"></i></button></div>');
+						+'<button class="input-group-text btnClip" id="btnSHA512" data-clipboard-text="'+ data.sha512 +'" data-bs-toggle="tooltip" data-bs-title="Copiar"><i class="fa-regular fa-copy"></i></button></div>');
 						
 						$('.card-data').append(div +'WHIRLPOOL</span><span class="form-control" id="whirlpool">'+ data.whirlpool +'</span>'
-						+'<button class="input-group-text btnClip" id="btnWHIRLPOOL" data-clipboard-text="'+ data.whirlpool +'"><i class="fa-regular fa-copy"></i></button></div>');
+						+'<button class="input-group-text btnClip" id="btnWHIRLPOOL" data-clipboard-text="'+ data.whirlpool +'" data-bs-toggle="tooltip" data-bs-title="Copiar"><i class="fa-regular fa-copy"></i></button></div>');
 
 					}, 100);
+					
+					setTimeout(function () {
+						const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
+						const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
+					}, 500);
+	
 					//SwalMensaje('Hash Generados', 'Se han generado los Hash HMAC.', 'success');
 				}else{
 					SwalMensaje('Hash no Generados', 'No se han generado los Hash HMAC.', 'error');
